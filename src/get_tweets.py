987aeb67ab82b0fcc70782a78b6b8edc171784d3
Sophia Ju <sophia.ju10@gmail.com>
2021-11-27 18:02:53 -0500

    added script for getting tweets along with the 10 sample tweets gotten

--- src/get_tweets.py
+++ src/get_tweets.py
@@ -1,1 +1,21 @@
-# script to get tweets
+import csv
+import tweepy
+
+def main():
+
+    client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAMgCWQEAAAAAhD5zT%2F0CSz5%2FvBFYw6ONa8zhtEY%3DLuIyhyXTaPWIzJzRzRz6TIv1Jhq9uNYRY1km6X8hDHTSLO5faI')
+
+    query = '(covid OR covid19 OR COVID OR COVID19 OR quarantine OR pandemic OR vaccine OR vaccination) lang:en -is:retweet'
+
+    tweets = client.search_recent_tweets(query=query, max_results=10)
+    # print(tweets)
+
+    output_file = '../data/filtered_tweets.tsv'
+
+    with open(output_file, 'w', encoding='utf-8', newline='') as file:
+        writer=csv.writer(file, delimiter='\t')
+        for tweet in tweets.data:
+            writer.writerow([tweet.id, tweet.text.replace('\n',' ').replace('\t', ' ')])
+
+if __name__ == '__main__':
+    main()
